e7b1232332aca6fb574c4b36b2915cbca7519e2e
Adam Simon <adampersimonal@gmail.com>
2022-06-25 17:34:01 -0500

    Added grid initialization

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,11 +1,39 @@
+import { useRef, useState, useEffect } from "react";
+
 // Styles
 
 import classes from "./styles.module.css";
 
 const Grid = (): JSX.Element => {
+  const gridRef = useRef<HTMLCanvasElement | null>(null);
+  const gridContextRef = useRef<CanvasRenderingContext2D | null>(null);
+
+  const [height, setHeight] = useState<number>(0);
+  const [width, setWidth] = useState<number>(0);
+
+  const tileSize: number = 16;
+  const gridScale: number = 4;
+
+  useEffect((): void => {
+    if (gridRef.current) {
+      setHeight(1);
+      setWidth(1);
+      gridContextRef.current = gridRef.current.getContext("2d");
+    }
+  }, []);
+
   return (
     <div className={classes.gridContainer}>
-      <canvas className={classes.grid}></canvas>
+      <canvas
+        className={classes.grid}
+        ref={gridRef}
+        height={height * tileSize}
+        width={width * tileSize}
+        style={{
+          height: height * tileSize * gridScale,
+          width: width * tileSize * gridScale,
+        }}
+      ></canvas>
     </div>
   );
 };
