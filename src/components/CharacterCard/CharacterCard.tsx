69b3d5c45189fc24b0b7bbaee64cf3dd4b10b60f
Adam Simon <adampersimonal@gmail.com>
2022-07-04 23:03:45 -0500

    Improved character card component

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -37,48 +37,60 @@ const CharacterCard = (props: CharacterCardProps): JSX.Element => {
     <div className={classes.characterCard}>
       <div className={classes.infoContainer}>
         <div className={classes.info}>
-          <div className={classes.label}>HP</div>
-          <div className={classes.value}>{health}</div>
+          <img
+            className={classes.infoImage}
+            src={require("../../assets/heart.png")}
+          />
+          <div className={classes.infoValue}>{health}</div>
         </div>
         <div className={classes.info}>
-          <div className={classes.label}>ATK</div>
-          <div className={classes.value}>{attack}</div>
+          <img
+            className={classes.infoImage}
+            src={require("../../assets/swords.png")}
+          />
+          <div className={classes.infoValue}>{attack}</div>
         </div>
         <div className={classes.portrait}>
           <img src={require("../../assets/person.png")} />
           <div className={classes.nameContainer}>{name}</div>
         </div>
         <div className={classes.info}>
-          <div className={classes.label}>DEF</div>
-          <div className={classes.value}>{defense}</div>
+          <img
+            className={classes.infoImage}
+            src={require("../../assets/shield.png")}
+          />
+          <div className={classes.infoValue}>{defense}</div>
         </div>
         <div className={classes.info}>
-          <div className={classes.label}>SPD</div>
-          <div className={classes.value}>{speed}</div>
+          <img
+            className={classes.infoImage}
+            src={require("../../assets/boots.png")}
+          />
+          <div className={classes.infoValue}>{speed}</div>
         </div>
       </div>
       <div className={classes.attributesContainer}>
-        <div className={classes.info}>
+        <div className={classes.attribute}>
           <div className={classes.label}>STR</div>
           <div className={classes.value}>{strength}</div>
         </div>
-        <div className={classes.info}>
+        <div className={classes.attribute}>
           <div className={classes.label}>DEX</div>
           <div className={classes.value}>{dexterity}</div>
         </div>
-        <div className={classes.info}>
+        <div className={classes.attribute}>
           <div className={classes.label}>CON</div>
           <div className={classes.value}>{constitution}</div>
         </div>
-        <div className={classes.info}>
+        <div className={classes.attribute}>
           <div className={classes.label}>INT</div>
           <div className={classes.value}>{intelligence}</div>
         </div>
-        <div className={classes.info}>
+        <div className={classes.attribute}>
           <div className={classes.label}>WIS</div>
           <div className={classes.value}>{wisdom}</div>
         </div>
-        <div className={classes.info}>
+        <div className={classes.attribute}>
           <div className={classes.label}>CHA</div>
           <div className={classes.value}>{charisma}</div>
         </div>
