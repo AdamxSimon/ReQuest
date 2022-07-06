658a16121e5e3a1091810cfe9e52614c9004dd46
Adam Simon <adampersimonal@gmail.com>
2022-07-05 22:24:17 -0500

    Added new character button

--- src/components/CharactersTool/CharactersTool.tsx
+++ src/components/CharactersTool/CharactersTool.tsx
@@ -9,6 +9,9 @@ import classes from "./styles.module.css";
 const CharactersTool = (): JSX.Element => {
   return (
     <div className={classes.toolContainer}>
+      <div className={classes.toolbar}>
+        <div className={classes.button}>New Character</div>
+      </div>
       <CharacterManagement />
     </div>
   );
